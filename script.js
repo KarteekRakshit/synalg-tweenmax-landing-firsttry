789343bce7dace58b5f194527828a3c812ec6757
Karteek Rakshit <karteek263@gmail.com>
2021-07-12 22:10:59 +0530

    adding content

--- script.js
+++ script.js
@@ -139,20 +139,20 @@ t1.from(".ringOne", 4, {
             ease: Expo.easeInOut
       }, "-=5");
 
-var textWrapper = document.querySelector('.ml7 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+// var textWrapper = document.querySelector('.ml7 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-anime.timeline({ loop: false })
-      .add({
-            targets: '.ml7 .letter',
-            translateY: ["1.1em", 0],
-            translateX: ["0.55em", 0],
-            translateZ: 0,
-            rotateZ: [180, 0],
-            duration: 750,
-            easing: "easeOutExpo",
-            delay: function (el, i) {
-                  return 9000 + 50 * i;
-            }
-      });
+// anime.timeline({ loop: false })
+//       .add({
+//             targets: '.ml7 .letter',
+//             translateY: ["1.1em", 0],
+//             translateX: ["0.55em", 0],
+//             translateZ: 0,
+//             rotateZ: [180, 0],
+//             duration: 750,
+//             easing: "easeOutExpo",
+//             delay: function (el, i) {
+//                   return 9000 + 50 * i;
+//             }
+//       });
 
